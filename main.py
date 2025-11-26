e160a45ce7c41da8938e3e0590bd7fd5eeaedfba
Vict0rNogueira <victorndecastro@outlook.com>
2025-11-26 11:48:39 -0300

    PréFinal

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/campeões.html')
-def campeões():
-    return render_template("campeões.html")
+@app.route('/campeoes.html')
+def campeoes():
+    return render_template("campeoes.html")
 
 @app.route('/Kayn.html')
 def Kayn():
     return render_template("Kayn.html")
+
+@app.route('/base.html')
+def base():
+    return render_template("base.html")
 
 app.run()
